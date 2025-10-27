8446b47b60787f0b6f45dbdd7ef007e1b8ad97dd
thetwoface <miketwoface@gmail.com>
2025-10-27 22:44:55 +0100

    character sheet skeleton tabs

--- src/module/actor/character-sheet-v2.mjs
+++ src/module/actor/character-sheet-v2.mjs
@@ -7,9 +7,75 @@ export default class ACKSCharacterSheetV2 extends ACKSActorSheetV2 {
   };
 
   /** @override */
+  static TABS = {
+    primary: {
+      tabs: [
+        { id: "attributes", label: "ACKS.category.attributes" },
+        { id: "abilities", label: "ACKS.category.abilities" },
+        { id: "spells", label: "ACKS.category.spells" },
+        { id: "inventory", label: "ACKS.category.inventory" },
+        { id: "notes", label: "ACKS.category.notes" },
+        { id: "hirelings", label: "ACKS.category.hirelings" },
+        { id: "effects", label: "ACKS.category.effects" },
+      ],
+      initial: "attributes",
+    },
+  };
+
+  /** @override */
+  tabGroups = {
+    primary: "attributes",
+  };
+
+  /** @override */
   static PARTS = {
     header: {
       template: "systems/acks/templates/actors/v2/header.hbs",
     },
+    tabs: {
+      template: "templates/generic/tab-navigation.hbs",
+    },
+    attributes: {
+      template: "systems/acks/templates/actors/v2/attributes.hbs",
+    },
+    abilities: {
+      template: "systems/acks/templates/actors/v2/abilities.hbs",
+    },
+    spells: {
+      template: "systems/acks/templates/actors/v2/spells.hbs",
+    },
+    inventory: {
+      template: "systems/acks/templates/actors/v2/inventory.hbs",
+    },
+    notes: {
+      template: "systems/acks/templates/actors/v2/notes.hbs",
+    },
+    hirelings: {
+      template: "systems/acks/templates/actors/v2/hirelings.hbs",
+    },
+    effects: {
+      template: "systems/acks/templates/actors/v2/effects.hbs",
+    },
   };
+
+  /**
+   * Prepare context that is specific to only a single rendered part.
+   *
+   * It is recommended to augment or mutate the shared context so that downstream methods like _onRender have
+   * visibility into the data that was used for rendering. It is acceptable to return a different context object
+   * rather than mutating the shared context at the expense of this transparency.
+   *
+   * @param {string} partId                         The part being rendered
+   * @param {ApplicationRenderContext} context      Shared context provided by _prepareContext
+   * @param {HandlebarsRenderOptions} options       Options which configure application rendering behavior
+   * @returns {Promise<ApplicationRenderContext>}   Context data for a specific part
+   * @protected
+   */
+  async _preparePartContext(partId, context, options) {
+    context = await super._preparePartContext(partId, context, options);
+
+    context.tab = context.tabs[partId];
+
+    return context;
+  }
 }
